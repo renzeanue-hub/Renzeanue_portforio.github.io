43a411d17305af8474ef098e2da8ef3bd55d7573
renzeanue-hub <renzeanue@gmail.com>
2026-01-09 11:49:19 +0900

    Create loding.js

---
+++ loding.js
@@ -0,0 +1,12 @@
+﻿const minLoadTime = 1000; // 最低1秒表示
+const startTime = Date.now();
+
+window.addEventListener('load', () => {
+  const loadTime = Date.now() - startTime;
+  const delay = Math.max(0, minLoadTime - loadTime);
+
+  setTimeout(() => {
+    document.getElementById('loading').classList.add('loaded');
+    document.getElementById('content').classList.add('show');
+  }, delay);
+});
