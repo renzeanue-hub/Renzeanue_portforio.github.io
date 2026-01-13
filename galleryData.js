9355f03a4e2e866fdd161a1100607c25dc21cefa
renzeanue-hub <renzeanue@gmail.com>
2026-01-13 15:56:14 +0900

    Create galleryData.js

---
+++ galleryData.js
@@ -0,0 +1,62 @@
+﻿const galleryData = [
+  {
+    id: "chino_prince",
+    src: "images/Chino_prince.png",
+    title: "Starry Sky Noble",
+    description: "CHINO(VALIS) Fanart 2025/7"
+  },
+  {
+    id: "valis_summer",
+    src: "images/VALIS_Summer.jpg",
+    title: "Summer Harmony",
+    description: "VALIS Fanart 2025/8"
+  },
+ {
+    id: "myu_carmen",
+    src: "images/Myu_Carmen.jpg",
+    title: "Fatal Scarlet",
+    description: "MYU(VALIS) Fanart 2025/7"
+  },
+  {
+    id: "rara_juice",
+    src: "images/Rara_JUICE.jpg",
+    title: "JUICE",
+    description: "RARA Fanart 2025/8"
+  },
+  {
+    id: "chino_memoria",
+    src: "images/CHINO_Memoria.jpg",
+    title: "Memoria",
+    description: "CHINO(VALIS) Fanart 2025/7"
+  },
+  {
+    id: "neffy_eyes_on_me",
+    src: "images/Neffy_EyesOnMe.jpg",
+    title: "Eyes On Me",
+    description: "NEFFY(VALIS) Fanart 2025/11"
+  },
+  {
+    id: "rara_juice",
+    src: "images/Rara_JUICE.jpg",
+    title: "JUICE",
+    description: "RARA Fanart 2025/5"
+  },
+  {
+    id: "vitte_black_swan",
+    src: "images/Vitte_BlackSwan.jpg",
+    title: "βlack Swan",
+    description: "VITTE(VALIS) Fanart 2025/6"
+  },
+ {
+    id: "valis_flowers",
+    src: "images/VALIS_flowers.jpg",
+    title: "Flowers of blessing",
+    description: "VALIS Fanart 2025/12"
+  },
+  {
+    id: "rara_moment",
+    src: "images/Rara_moment.jpg",
+    title: "This moment!",
+    description: "RARA(VALIS) Fanart 2025/8"
+  }
+];
