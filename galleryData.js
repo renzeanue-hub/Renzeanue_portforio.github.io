e54e20a98c130690f558057dc0ea1f8043f17f54
renzeanue-hub <renzeanue@gmail.com>
2026-01-14 11:23:36 +0900

    Update galleryData.js

--- galleryData.js
+++ galleryData.js
@@ -3,60 +3,70 @@
     id: "chino_prince",
     src: "images/Chino_prince.png",
     title: "Starry Sky Noble",
-    description: "CHINO(VALIS) Fanart 2025/7"
+    description: "CHINO(VALIS) Fanart 2025/7",
+    detail_description: ""
   },
   {
     id: "valis_summer",
     src: "images/VALIS_Summer.jpg",
     title: "Summer Harmony",
-    description: "VALIS Fanart 2025/8"
+    description: "VALIS Fanart 2025/8",
+    detail_description: ""
   },
  {
     id: "myu_carmen",
     src: "images/Myu_Carmen.jpg",
     title: "Fatal Scarlet",
-    description: "MYU(VALIS) Fanart 2025/7"
+    description: "MYU(VALIS) Fanart 2025/7",
+    detail_description: ""
   },
   {
     id: "rara_juice",
     src: "images/Rara_JUICE.jpg",
     title: "JUICE",
-    description: "RARA Fanart 2025/8"
+    description: "RARA Fanart 2025/8",
+    detail_description: ""
   },
   {
     id: "chino_memoria",
     src: "images/CHINO_Memoria.jpg",
     title: "Memoria",
-    description: "CHINO(VALIS) Fanart 2025/7"
+    description: "CHINO(VALIS) Fanart 2025/7",
+    detail_description: "VALISのメンバー、CHINOのオリジナルソロ曲「Memoria」のファンアートです。\n時間とともに薄れゆく思い出。それに抗うように、補い、反芻しながら、人は過去を留めようとするのかもしれない。\nけれど、そんな独りよがりで描き出した記憶は結局、贋物にすぎないのだろうか。\nそれでも私は、忘れたくない。"
   },
   {
-    id: "neffy_eyes_on_me",
+    id: "neffy_eyesonme",
     src: "images/Neffy_EyesOnMe.jpg",
     title: "Eyes On Me",
-    description: "NEFFY(VALIS) Fanart 2025/11"
+    description: "NEFFY(VALIS) Fanart 2025/11",
+    detail_description: "VALISのメンバー、NEFFYのオリジナルソロ曲「Eyes On Me」のファンアートです。"
   },
   {
     id: "rara_juice",
     src: "images/Rara_JUICE.jpg",
     title: "JUICE",
-    description: "RARA Fanart 2025/5"
+    description: "RARA Fanart 2025/5",
+    detail_description: ""
   },
   {
-    id: "vitte_black_swan",
+    id: "vitte_blackswan",
     src: "images/Vitte_BlackSwan.jpg",
     title: "βlack Swan",
-    description: "VITTE(VALIS) Fanart 2025/6"
+    description: "VITTE(VALIS) Fanart 2025/6",
+    detail_description: ""
   },
  {
     id: "valis_flowers",
     src: "images/VALIS_flowers.jpg",
     title: "Flowers of blessing",
-    description: "VALIS Fanart 2025/12"
+    description: "VALIS Fanart 2025/12",
+    detail_description: ""
   },
   {
     id: "rara_moment",
     src: "images/Rara_moment.jpg",
     title: "This moment!",
-    description: "RARA(VALIS) Fanart 2025/8"
+    description: "RARA(VALIS) Fanart 2025/8",
+    detail_description: ""
   }
 ];
