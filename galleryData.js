6e3300472cf7ba5a260cfa06df8f9b15215c93fb
renzeanue-hub <renzeanue@gmail.com>
2026-01-15 17:57:06 +0900

    Update galleryData.js

--- galleryData.js
+++ galleryData.js
@@ -1,14 +1,16 @@
 ﻿const galleryData = [
   {
     id: "chino_memoria",
+    thumbnail: "images/thumbnail/CHINO_Memoria_thumbnail.jpg",
     src: "images/CHINO_Memoria.jpg",
     title: "Memoria",
     category: "fanart",
     description: "CHINO(VALIS) Fanart 2025/7",
-    detail_description: "VALISのメンバー、CHINOのオリジナルソロ曲「Memoria」のファンアートです。\n時間とともに薄れゆく思い出。それに抗うように、補い、反芻しながら、人は過去を留めようとするのかもしれない。\nけれど、そんな独りよがりで描き出した記憶は結局、贋物にすぎないのだろうか。\nそれでも私は、忘れたくない。"
+    detail_description: "VALISのメンバー、CHINOのオリジナルソロ曲「Memoria」のファンアートです。\n時間とともに薄れゆく思い出。それに抗うように、補い、反芻しながら、人は過去を留めようとするのかもしれない。\nけれど、そんな独りよがりで描き出した記憶は結局、贋物にすぎないのだろうか。\nそれでも私は忘れたくない。"
   },
   {
     id: "neffy_eyesonme",
+    thumbnail: "images/thumbnail/Neffy_EyesOnMe_thumbnail.jpg",
     src: "images/Neffy_EyesOnMe.jpg",
     title: "Eyes On Me",
     category: "fanart",
@@ -17,22 +19,25 @@
   },
   {
     id: "rara_juice",
+    thumbnail: "images/thumbnail/Rara_JUICE_thumbnail.jpg",
     src: "images/Rara_JUICE.jpg",
     title: "JUICE",
     category: "fanart",
     description: "RARA Fanart 2025/5",
-    detail_description: ""
+    detail_description: "VALISのメンバー、RARAのオリジナルソロ曲「JUICE」のファンアートです。"
   },
   {
     id: "vitte_blackswan",
+    thumbnail: "images/thumbnail/Vitte_BlackSwan_thumbnail.jpg",
     src: "images/Vitte_BlackSwan.jpg",
     title: "βlack Swan",
     category: "fanart",
     description: "VITTE(VALIS) Fanart 2025/6",
-    detail_description: ""
+    detail_description: "VALISのメンバー、VITTEのオリジナルソロ曲「βlack Swan」のファンアートです。黒鳥として生まれてしまった王女が、孤独と葛藤の末に憧れを捨て、彼女だけの黒い輝きを放つ決意を描きました。"
   },
   {
     id: "chino_prince",
+    thumbnail: "images/thumbnail/Chino_prince_thumbnail.png",
     src: "images/Chino_prince.png",
     title: "Starry Sky Noble",
     category: "fanart",
@@ -41,15 +46,17 @@
   },
   {
     id: "valis_flowers",
+    thumbnail: "images/thumbnail/VALIS_flowers_thumbnail.jpg",
     src: "images/VALIS_flowers.jpg",
     title: "Flowers",
     category: "fanart",
     description: "VALIS Fanart 2025/12",
-    detail_description: ""
+    detail_description: "VALISのメンバー全員を描いたファンアートです。VALIS 7th ONE-MAN LIVE「彷徨フォーエバー」のフラワースタンド企画にて使用されたイラストです。"
   },
 
  {
     id: "myu_carmen",
+    thumbnail: "images/thumbnail/Myu_Carmen_thumbnail.jpg",
     src: "images/Myu_Carmen.jpg",
     title: "Fatal Scarlet",
     category: "fanart",
@@ -58,6 +65,7 @@
   },
   {
     id: "rara_moment",
+    thumbnail: "images/thumbnail/Rara_moment_thumbnail.jpg",
     src: "images/Rara_moment.jpg",
     title: "This moment!",
     category: "fanart",
@@ -66,10 +74,11 @@
   },
   {
     id: "valis_summer",
+    thumbnail: "images/thumbnail/VALIS_Summer_thumbnail.jpg",
     src: "images/VALIS_Summer.jpg",
-    title: "Summer Harmony",
+    title: "Hot Girls Summer!!",
     category: "fanart",
-    description: "VALIS Fanart 2025/8",
-    detail_description: ""
+    description: "VALIS Fanart 2025/9",
+    detail_description: "夏VALIS"
   },
 ];
